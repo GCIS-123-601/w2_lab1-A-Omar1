049778880707ddc352449aa66c44504f0fe3a7b3
A-Omar1 <90127409+A-Omar1@users.noreply.github.com>
2021-09-19 19:15:23 +0400

    Add files via upload

---
+++ Omar_Abdelhamid_lab2_both programs.py
@@ -0,0 +1,24 @@
+import turtle
+def testDrive():
+    turtle.forward(180)
+    turtle.left(87)
+    turtle.setheading(127)
+    turtle.up()
+    turtle.goto(50,40)
+    turtle.home()
+    turtle.down()
+    turtle.circle(25)
+def turtleState():
+    v2 = turtle.isdown()
+    v3 = turtle.heading()
+    tXcor = turtle.xcor()
+    tYcor = turtle.ycor()
+    print("turtle is down?", v2)
+    print("turtle heading:", v3)
+    print("xcor: ",tXcor, "ycor: ", tYcor)
+def main():
+    testDrive()
+    turtleState()
+
+main()
+print ("press Enter to continue...")
